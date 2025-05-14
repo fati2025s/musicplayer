d72be40dc2118887ae367371cd2d389a38b9fad0
Amirabbas-Amadeh <amirabbasamadeh1@gmail.com>
2025-05-14 19:15:05 +0330

    Add files via upload

---
+++ UserManager.java
@@ -0,0 +1,21 @@
+import User;
+
+public class UserManager {
+
+    public User register(String username, String email, String password) {
+        return null;
+    }
+
+    public boolean confirm(String email, String password) {
+    }
+
+    public boolean deleteAccount(User user) {
+    }
+
+    public boolean changePassword(User user, String newPassword) {
+    }
+
+    public boolean isEmailTaken(String email) {
+        return false;
+    }
+}
